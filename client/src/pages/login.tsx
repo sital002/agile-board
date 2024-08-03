c210710af4ad66c5a579e815940b9585aaca03cb
saroj <sarojaryal2003@gmail.com>
2024-08-03 18:11:10 +0545

    add login and signup form

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,104 +1,79 @@
-import { useState } from "react";
-import { Button } from "../components/ui/button";
-import { Card } from "../components/ui/card";
-import { Input } from "../components/ui/input";
-import { z } from "zod";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { FacebookIcon, GithubIcon } from "lucide-react";
+"use client"
 
-const loginSchema = z.object({
-  email: z.string().email({
-    message: "Invaild Email",
+import { SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
+import { Button } from "../../src/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../src/components/ui/form"
+import { Input } from "../../src/components/ui/input"
+
+const formSchema = z.object({
+  email: z.string().email({message:"invalid email"}).min(2, {
+    message: "Username must be at least 2 characters.",
   }),
-  password: z
-    .string({
-      required_error: "Password is required",
-    })
-    .trim()
-    .min(8, "Password must be at least 8 characters")
-    .max(64, "Password must be less than 64 characters"),
-});
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  })
+})
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+type FormInputType=z.infer<typeof formSchema>
 
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    // console.log(email, password);
-    try {
-      const data = loginSchema.parse({ email, password });
-      console.log(data);
-      setEmailError("");
-      setPasswordError("");
-      const res = await axios.post("http://localhost:3001/auth/login", data);
-      console.log(res.data);
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        error.errors.map((err) => {
-          console.log(err.path[0]);
-          if (err.path[0] === "email") {
-            setEmailError(err.message);
-          }
-          if (err.path[0] === "password") {
-            setPasswordError(err.message);
-          }
-        });
-        // console.log(error.errors);
-        // setEmailError(error.errors[0].message);
-        // setPasswordError(error.errors[1].message);
-      }
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
-    }
-  }
-  return (
-    <div className="flex justify-center h-screen items-center ">
-      <Card className="p-3 w-full max-w-[600px]">
-        <form onSubmit={handleSubmit}>
-          <h2 className="text-xl text-center">Sign In</h2>
-          <Input
-            placeholder="Email"
-            className="my-3"
-            onChange={(e) => setEmail(e.currentTarget.value)}
-          />
-          <p className="text-destructive my-2">{emailError}</p>
-          <Input
-            placeholder="Password"
-            className="my-3"
-            type="password"
-            onChange={(e) => setPassword(e.currentTarget.value)}
-          />
-          {passwordError && (
-            <p className="text-destructive my-2">{passwordError}</p>
-          )}
-          <div className="flex justify-center my-3 gap-2">
-            <GithubIcon
-              size={33}
-              className="border rounded-lg p-1 cursor-pointer"
-            />
-            <FacebookIcon
-              size={33}
-              className="border rounded-lg p-1 cursor-pointer"
-            />
-            <GithubIcon
-              size={33}
-              className="border rounded-lg p-1 cursor-pointer"
-            />
-          </div>
-          <Button className="w-full">Sign In</Button>
-        </form>
-        <p className="text-center py-3">
-          Don't have an account?{" "}
-          <Link to={"/signup"} className="underline">
-            Sign up
-          </Link>
-        </p>
-      </Card>
-    </div>
-  );
+const onSubmit:SubmitHandler<FormInputType>=async(data)=>{
+  console.log(data)
 }
+
+export function Signup() {
+  const form=useForm<FormInputType>({
+    resolver:zodResolver(formSchema)
+  })
+
+  return (
+   <div className="w-full mt-[10%] max-w-[30%] mx-auto border-2 p-3 rounded-md">
+    <h1 className="text-4xl font-semibold text-center my-2">Login Form</h1>
+     <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+       
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="enter your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="enter your password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+       
+        <Button className="w-full" type="submit">Login</Button>
+      </form>
+    </Form>
+   </div>
+  )
+}
+
+
+export default Signup

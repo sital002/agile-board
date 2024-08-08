bca6215ba5e3a6b1dc9c400550973dbcd5b4950d
Sital Adhikari <sitaladhikari002@gmail.com>
2024-08-08 16:19:08 +0545

    update axios config

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL: env.VITE_SERVER_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     "x-access-server": `${localStorage.getItem("access_token")}`,

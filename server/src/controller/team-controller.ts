d307b17dc866faeb5847fce675d20c901465e87e
Sital Adhikari <sitaladhikari002@gmail.com>
2024-08-25 13:22:05 +0545

    refactor:team controller

--- server/src/controller/team-controller.ts
+++ server/src/controller/team-controller.ts
@@ -1,62 +1,55 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import { asyncHandler } from "../utils/AsyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
 
 const prisma = new PrismaClient();
 
-export async function getTeams(req: Request, res: Response) {
-  try {
-    const projectId = req.params.projectId || req.body.projectId;
-    if (!projectId)
-      return res.status(400).json({ error: "Project ID is required" });
+export const getTeams = asyncHandler(async (req: Request, res: Response) => {
+  const projectId = req.params.projectId || req.body.projectId;
+  if (!projectId) throw new ApiError(400, "Project ID is required");
 
-    const teams = await prisma.team.findFirst({
-      where: {
-        projectId,
-      },
-      include: {
-        members: true,
-      },
-    });
-    return res.status(200).json(teams?.members || []);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: "An error occurred" });
-  }
-}
+  const teams = await prisma.team.findFirst({
+    where: {
+      projectId,
+    },
+    include: {
+      members: true,
+    },
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse("Teams fetched successfully", teams || []));
+});
 
-export async function deleteTeam(req: Request, res: Response) {
-  try {
-    const id = req.params.id;
-    if (!id) return res.status(400).json({ error: "Team IDj is required" });
+export const deleteTeam = asyncHandler(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  if (!id) throw new ApiError(400, "Team ID is required");
 
-    const team = await prisma.team.delete({
-      where: {
-        id,
-      },
-    });
-    if (team) {
-      return res.status(200).json(team);
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: "An error occurred" });
-  }
-}
+  const team = await prisma.team.delete({
+    where: {
+      id,
+    },
+  });
+  if (!team) throw new ApiError(500, "Failed to delete team");
+  return res
+    .status(200)
+    .json(new ApiResponse("Team deleted successfully", team));
+});
 
-export async function updateTeamMembers(req: Request, res: Response) {
-  try {
-    if (!req.user)
-      return res.status(400).json({ error: "You arenot loggedin" });
+export const updateTeamMembers = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) throw new ApiError(400, "You are not logged in");
     const id = req.params.projectId;
     const { email } = req.body;
-    if (!email) return res.status(400).json({ error: "Members are required" });
+    if (!email) throw new ApiError(400, "Email is required");
     const memberExists = await prisma.user.findFirst({
       where: {
         email: email,
       },
     });
-    if (!memberExists)
-      return res.status(400).json({ error: "Member does not exist" });
+    if (!memberExists) throw new ApiError(404, "Member not found");
 
     const project = await prisma.project.findFirst({
       where: {
@@ -66,9 +59,9 @@ export async function updateTeamMembers(req: Request, res: Response) {
         creator: true,
       },
     });
-    if (project?.creator.id !== req.user.id) {
-      return res.status(400).json({ error: "You aren't the creator" });
-    }
+    if (!project) throw new ApiError(404, "Project not found");
+    if (project.creator.id !== req.user.id)
+      throw new ApiError(400, "Unauthorized");
     const team = await prisma.team.update({
       where: {
         projectId: id,
@@ -81,21 +74,18 @@ export async function updateTeamMembers(req: Request, res: Response) {
         },
       },
     });
-    if (team) {
-      return res.status(200).json(team);
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: "An error occurred" });
+    if (!team) throw new ApiError(500, "Failed to add member to team");
+    return res.status(200).json(new ApiResponse("Member added", team));
   }
-}
+);
 
-export async function removeTeammember(req: Request, res: Response) {
-  try {
+export const removeTeammember = asyncHandler(
+  async (req: Request, res: Response) => {
     const id = req.params.projectId;
-
+    if (!req.user) throw new ApiError(400, "You are not logged in");
+    if (!id) throw new ApiError(400, "Project ID is required");
     const { member } = req.body;
-    if (!member) return res.status(400).json({ error: "Members are required" });
+    if (!member) throw new ApiError(400, "Member is required");
 
     const project = await prisma.project.findFirst({
       where: {
@@ -105,9 +95,9 @@ export async function removeTeammember(req: Request, res: Response) {
         creator: true,
       },
     });
-    if (project?.creator.id !== req.user?.id) {
-      return res.status(400).json({ error: "Unauthorized" });
-    }
+    if (!project) throw new ApiError(404, "Project not found");
+    if (project.creator.id !== req.user.id)
+      throw new ApiError(400, "Unauthorized");
     const team = await prisma.team.update({
       where: {
         id,
@@ -118,11 +108,7 @@ export async function removeTeammember(req: Request, res: Response) {
         },
       },
     });
-    if (team) {
-      return res.status(200).json(team);
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: "An error occurred" });
+    if (!team) throw new ApiError(500, "Failed to remove member from team");
+    return res.status(200).json(new ApiResponse("Member removed", team));
   }
-}
+);

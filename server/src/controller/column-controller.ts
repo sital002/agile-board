0fecb91c9255e261d51cb7bc1961e738b04f39d7
Sital Adhikari <sitaladhikari002@gmail.com>
2024-08-25 13:07:39 +0545

    refactor column controller

--- server/src/controller/column-controller.ts
+++ server/src/controller/column-controller.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import { asyncHandler } from "../utils/AsyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
 
 const prisma = new PrismaClient();
-export async function createColumn(req: Request, res: Response) {
-  try {
-    if (!req.user) return res.status(400).json({ error: "Unathorized" });
+export const createColumn = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) throw new ApiError(400, "You are not logged in");
     const { name, projectId } = req.body;
     const column = await prisma.column.create({
       data: {
@@ -13,60 +16,51 @@ export async function createColumn(req: Request, res: Response) {
       },
     });
     if (column) {
-      res.status(201).json(column);
+      res
+        .status(201)
+        .json(new ApiResponse("Column created successfully", column));
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "An error occurred" });
   }
-}
+);
 
-export async function getColumns(req: Request, res: Response) {
-  try {
-    if (!req.user) return res.status(400).json({ error: "Unathorized" });
-    const projectId = req.params.projectId;
-    if (!projectId)
-      return res.status(400).json({ error: "Project Id is required" });
-
-    const columns = await prisma.column.findMany({
-      where: {
-        projectId: projectId,
-      },
-    });
-    if (columns) {
-      res.status(200).json(columns);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "An error occurred" });
-  }
-}
-
-export async function deleteColumn(req: Request, res: Response) {
-  try {
-    if (!req.user) return res.status(400).json({ error: "Unathorized" });
+export const getColumns = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.user) throw new ApiError(400, "You are not logged in");
+  const projectId = req.params.projectId;
+  if (!projectId) throw new ApiError(400, "Project ID is required");
+  const columns = await prisma.column.findMany({
+    where: {
+      projectId: projectId,
+    },
+  });
+  if (!columns) throw new ApiError(404, "No columns found");
+  res
+    .status(200)
+    .json(new ApiResponse("Columns retrieved successfully", columns));
+});
+export const deleteColumn = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) throw new ApiError(400, "You are not logged in");
     const id = req.params.id;
-    if (!id) return res.status(400).json({ error: "Column ID is required" });
+    if (!id) throw new ApiError(400, "Column ID is required");
 
     const column = await prisma.column.delete({
       where: {
         id,
       },
     });
-    if (column) {
-      res.status(200).json(column);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "An error occurred" });
-  }
-}
 
-export async function updateColumn(req: Request, res: Response) {
-  try {
-    if (!req.user) return res.status(400).json({ error: "Unathorized" });
+    if (!column) throw new ApiError(500, "Failed to delete column");
+    res
+      .status(200)
+      .json(new ApiResponse("Column deleted successfully", column));
+  }
+);
+
+export const updateColumn = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) throw new ApiError(400, "You are not logged in");
     const id = req.params.id;
-    if (!id) return res.status(400).json({ error: "Column ID is required" });
+    if (!id) throw new ApiError(400, "Column ID is required");
     const { name } = req.body;
     const column = await prisma.column.update({
       where: {
@@ -76,11 +70,9 @@ export async function updateColumn(req: Request, res: Response) {
         name,
       },
     });
-    if (column) {
-      res.status(200).json(column);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "An error occurred" });
+    if (!column) throw new ApiError(500, "Failed to update column");
+    res
+      .status(200)
+      .json(new ApiResponse("Column updated successfully", column));
   }
-}
+);
